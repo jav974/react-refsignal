dbb0c8c01d442de62dcfa5430362e63d1f078f6c
jav974 <jeremyvienne@gmail.com>
2025-06-08 18:59:11 +0200

    added eslint and prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ),
+
+    plugins: {
+        react,
+        "@typescript-eslint": typescriptEslint,
+        prettier,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.jest,
+        },
+
+        parser: tsParser,
+    },
+
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+}]);
