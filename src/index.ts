47d7a35dfbacea018d41367b22696fc96f51b30a
jav974 <jeremyvienne@gmail.com>
2025-06-08 18:07:51 +0200

    config for clean build

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./hooks/useRefSignal";
+export * from "./hooks/useRefSignalEffect";
+export * from "./hooks/useRefSignalMemo";
+export * from "./hooks/useRefSignalRender";
+export { Listener, RefSignal, batch, createRefSignal } from "./refsignal";
