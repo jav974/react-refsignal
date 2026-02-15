fd20b3671330fa7276e2c8422eb411cc5bfc9745
jav974 <jeremyvienne@gmail.com>
2026-02-15 14:08:06 +0100

    export helper function isRefSignal

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ export * from './hooks/useRefSignal';
 export * from './hooks/useRefSignalEffect';
 export * from './hooks/useRefSignalMemo';
 export * from './hooks/useRefSignalRender';
-export { Listener, RefSignal, batch, createRefSignal } from './refsignal';
+export { Listener, RefSignal, batch, createRefSignal, isRefSignal } from './refsignal';
 export { configureDevTools, devtools } from './devtools';
 export type { DevToolsConfig, SignalUpdate } from './devtools';
